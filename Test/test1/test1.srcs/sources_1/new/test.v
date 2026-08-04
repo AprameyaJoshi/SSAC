e37a724f23a5cb7311aca9bc408c8c4581793a46
AprameyaJoshi <joshiaprameya16@gmail.com>
2026-08-04 23:32:01 +0530

    test1

---
+++ Test/test1/test1.srcs/sources_1/new/test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.08.2026 23:25:31
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test;
+initial
+$display("Testing.....");
+endmodule
